4b4e224b0b3f46a8dfa9b5f3992ec885df77ee56
liz <marvellousmakuyana@gmail.com>
2022-03-23 22:12:12 +0200

    edit

--- routes/api/send_email.js
+++ routes/api/send_email.js
@@ -1,35 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-router.get('/', (req, res) => {
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-            clientId: process.env.OAUTH_CLIENTID,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.OAUTH_REFRESH_TOKEN
-        }
-    });
-    
-    let mailOptions ={
-        from: 'marve5050@gmail.com',
-        to: 'marvellousmakuyana@gmail.com, marve5050@gmail.com',
-        subject: 'Test',
-        text: 'Hello world... Sent from Node.js'
-    };
-    
-    transporter.sendMail(mailOptions, function(error, data){
-        if(error){
-            console.log('Error ' + error);
-        } else{
-            res.send('Email sent successfully');
-        }
-    });
-    
+router.get("/", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+
+  let mailOptions = {
+    from: "example@gmail.com",
+    to: "example@gmail.com, example@gmail.com",
+    subject: "Test",
+    text: "Hello world... Sent from Node.js",
+  };
+
+  transporter.sendMail(mailOptions, function (error, data) {
+    if (error) {
+      console.log("Error " + error);
+    } else {
+      res.send("Email sent successfully");
+    }
+  });
 });
 
 module.exports = router;
